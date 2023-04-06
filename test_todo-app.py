320c6b5b9fb80a09f1bd03a34b4d1d03f9b82899
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-04-06 19:32:48 +0300

    Add test case for addTODO method

--- test_todo-app.py
+++ test_todo-app.py
@@ -1,11 +1,13 @@
 import os
 import unittest
+import jsonpickle
 import app as TODOApp
+import time
 
 class TestApp(unittest.TestCase):
 
     def setUp(self):
-        print('setUp')
+        print('\nsetUp')
 
         self.app = TODOApp.App(os.getcwd())
         self.appFileName = './app.py'
@@ -22,9 +24,33 @@ class TestApp(unittest.TestCase):
 
     def test_addTask(self):
         taskCount1 = len(self.app.taskList)
-        self.app.addTask('TestingTask')
+        task = TODOApp.TaskDescription('TestingTask', os.getcwd(), time.time())
+        self.app.addTask(task)
         taskCount2= len(self.app.taskList)
         self.assertLess(taskCount1, taskCount2)
+
+    def test_addTODO(self):
+        if self.app.taskCount < 1:
+            task = TODOApp.TaskDescription('TestingTask', os.getcwd(), time.time())
+            self.app.addTask(task)
+
+        todoCount1 = 0
+        for task in self.app.taskList:
+            todoCount1 += len(task.todos)
+
+        print('TODO count before adding = %d' % todoCount1)
+
+        self.app.addTODO(0, 'TestingTodo')
+
+        todoCount2 = 0
+        for task in self.app.taskList:
+            todoCount2 += len(task.todos)
+        
+        print('TODO count after adding = %d' % todoCount2)
+
+        self.assertLess(todoCount1, todoCount2)
+
+
 
 if __name__ == '__main__':
     unittest.main()
