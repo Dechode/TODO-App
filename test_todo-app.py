4a42c505e3878d82060469357a6438365982e2b2
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-04-06 21:01:21 +0300

    Add test case for adding and removing a todo

--- test_todo-app.py
+++ test_todo-app.py
@@ -29,6 +29,17 @@ class TestTODOApp(unittest.TestCase):
         taskCount2= len(self.app.taskList)
         self.assertLess(taskCount1, taskCount2)
 
+    def test_addNRemoveTask(self):
+        taskCount1 = len(self.app.taskList)
+        task = TODOApp.TaskDescription('TestingTask', os.getcwd(), time.time())
+        self.app.addTask(task)
+        taskCount2= len(self.app.taskList)
+        self.assertLess(taskCount1, taskCount2)
+
+        self.app.removeTask(0)
+        taskCount3 = len(self.app.taskList)
+        self.assertGreater(taskCount2, taskCount3)
+
     def test_addTODO(self):
         if self.app.taskCount < 1:
             task = TODOApp.TaskDescription('TestingTask', os.getcwd(), time.time())
@@ -38,7 +49,22 @@ class TestTODOApp(unittest.TestCase):
         for task in self.app.taskList:
             todoCount1 += len(task.todos)
 
-        print('TODO count before adding = %d' % todoCount1)
+        self.app.addTODO(0, 'TestingTodo')
+
+        todoCount2 = 0
+        for task in self.app.taskList:
+            todoCount2 += len(task.todos)
+        
+        self.assertLess(todoCount1, todoCount2)
+
+    def test_addNRemoveTODO(self):
+        if self.app.taskCount < 1:
+            task = TODOApp.TaskDescription('TestingTask', os.getcwd(), time.time())
+            self.app.addTask(task)
+
+        todoCount1 = 0
+        for task in self.app.taskList:
+            todoCount1 += len(task.todos)
 
         self.app.addTODO(0, 'TestingTodo')
 
@@ -46,11 +72,15 @@ class TestTODOApp(unittest.TestCase):
         for task in self.app.taskList:
             todoCount2 += len(task.todos)
         
-        print('TODO count after adding = %d' % todoCount2)
-
         self.assertLess(todoCount1, todoCount2)
 
+        self.app.removeTODO(0,0)
 
+        todoCount3 = 0
+        for task in self.app.taskList:
+            todoCount3 += len(task.todos)
+
+        self.assertGreater(todoCount2, todoCount3)
 
 if __name__ == '__main__':
     unittest.main()
